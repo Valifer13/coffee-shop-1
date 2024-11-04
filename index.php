b8322069c06b405d67bb524bc8d53cfbeb1882dd
Valifer13 <vianviszello@gmail.com>
2024-11-04 13:45:57 +0800

    Initialize Project

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'><![endif]-->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+	<meta name="author" content="Egprojets">
+	<meta name="description" content="" />
+	<title>Cafe | Home</title>
+	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+	
+	<!-- Google Fonts -->
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Oswald:400,700,300" />
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Architects+Daughter" />
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,300" />
+	<!-- End Google Fonts -->
+
+	<!-- Contribute CSS Files -->
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+	<link rel="stylesheet" href="assets/css/font-awesome.min.css" />
+	<link rel="stylesheet" href="assets/css/flaticon.css" />
+	<!-- Contribute End CSS Files -->
+
+	<!-- Custom CSS Files -->
+	<link rel="stylesheet" href="assets/css/style.css" />
+	<link rel="stylesheet" href="assets/css/responsive.css" />
+	<!-- Custom CSS Files -->
+
+	<!--[if lt IE 9]>
+		<script src="assets/js/html5shiv.js"></script>
+		<script src="assets/js/respond.min.js"></script>
+		<div class="bg-danger">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/" class="highlight">upgrade your browser</a> to improve your experience.</div>
+	<![endif]-->
+
+</head>
+
+<body>
+
+</body>
+
+</html>
