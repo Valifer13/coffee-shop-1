731f4d6c4d3a40061907de15ec4892fcedc77bc3
Valifer13 <vianviszello@gmail.com>
2024-11-04 22:02:00 +0800

    change and complete Navbar components

---
+++ assets/inc/Navbar.php
@@ -0,0 +1,59 @@
+<?php
+
+$Navbar = "
+	<!-- Loader Block -->
+	<div class=\"site-loader\">
+		<div class=\"loading\"></div>
+	</div>
+	<!-- End Loader Block -->
+
+	<!-- Site Wrapper -->
+	<div id=\"site-wrapper\">
+		<!-- Header -->
+		<header id=\"site-header\">
+			<div class=\"nav-top\">
+				<div class=\"container\">
+					<div class=\"row\">
+						<div class=\"col-sm-12\">
+							<ul>
+								<li><i class=\"fa fa-envira\"></i>Jam Buka - 09 am - 06 pm</li>
+								<li><i class=\"fa fa-phone\"></i>+62 831 1958 7745</li>
+								<li><a href=\"\"><i class=\"fa fa-envelope\"></i>info@redsystem.id</a></li>
+								<li class=\"social-bloc\">
+									<a href=\"#\"><i class=\"fa fa-twitter\"></i></a>
+									<a href=\"#\"><i class=\"fa fa-facebook\"></i></a>
+									<a href=\"#\"><i class=\"fa fa-google-plus\"></i></a>
+									<a href=\"#\"><i class=\"fa fa-skype\"></i></a>
+									<a href=\"#\"><i class=\"fa fa-instagram\"></i></a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class=\"navbar\" role=\"navigation\">
+				<div class=\"container\">
+					<div class=\"row\">
+						<h1 class=\"sr-only\">Coffee Shop</h1>
+						<a href=\"index.html\" title=\"FoodLover\" class=\"logo\">
+							<img src=\"assets/img/logo.png\" alt=\"\" width=\"170\">
+						</a>
+						<button data-target=\".navbar-collapse\" data-toggle=\"collapse\" type=\"button\" class=\"menu-mobile visible-xs\">
+							<span class=\"icon-bar\"></span>
+							<span class=\"icon-bar\"></span>
+							<span class=\"icon-bar\"></span>
+						</button>
+						<ul class=\"nav navbar-nav navbar-collapse collapse\">
+							<li><a class=\"active\" href=\"index.html\">Beranda</a></li>
+							<li><a href=\"about-us.html\">Tentang Kami</a></li>
+							<li><a href=\"menu.html\">Menu</a></li>
+							<li><a href=\"reservation.html\">Reservasi</a></li>
+							<li><a href=\"blog.html\">Blog</a></li>
+							<li><a href=\"contact-us.html\">Kontak Kami</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</header>
+		<!-- End Header -->
+";
